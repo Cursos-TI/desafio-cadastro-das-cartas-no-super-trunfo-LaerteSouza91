29abe3b5c82f6ae6f0779f41b400c155b914879a
LaerteSouza91 <laertesouza74@gmail.com>
2025-03-05 23:37:05 +0000

    ProgramaOK

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,23 +1,49 @@
 #include <stdio.h>
 
-int main(){
+int main() {
+    char nome1[50], nome2[50];
+    long pop1, pop2;
+    float area1, area2, pib1, pib2;
+    int pontos1, pontos2;
 
-    printf("Jogo Super Trunfo");
+    // Entrada de dados para o primeiro país
+    printf("Digite o nome do primeiro país: ");
+    scanf(" %[^\n]", nome1);
 
-    //variaveis das cartas Super Trunfo Países
-    
-    //Nome dos Países
-    char pais;
-    //Código de cada pais
-    int P01, P02;
-    //População total de cada País
-    int popu1, popu2;
-    //PIB de cada País
-    float pib1, pib2;
+    printf("Digite a população (em milhões): ");
+    scanf("%ld", &pop1);
 
-    scanf("Mostre a carta Brasil:")
+    printf("Digite a área (em milhões de km²): ");
+    scanf("%f", &area1);
 
+    printf("Digite o PIB (em trilhões de dólares): ");
+    scanf("%f", &pib1);
 
+    printf("Digite o número de pontos turísticos famosos: ");
+    scanf("%d", &pontos1);
+
+    // Entrada de dados para o segundo país
+    printf("\nDigite o nome do segundo país: ");
+    scanf(" %[^\n]", nome2);
+
+    printf("Digite a população (em milhões): ");
+    scanf("%ld", &pop2);
+
+    printf("Digite a área (em milhões de km²): ");
+    scanf("%f", &area2);
+
+    printf("Digite o PIB (em trilhões de dólares): ");
+    scanf("%f", &pib2);
+
+    printf("Digite o número de pontos turísticos famosos: ");
+    scanf("%d", &pontos2);
+
+    // Exibindo as cartas dos países
+    printf("\n=== Cartas dos Países ===\n");
+    printf("\n%s\nPopulação: %ld milhões\nÁrea: %.2f milhões de km²\nPIB: %.2f trilhões\nPontos turísticos: %d\n",
+           nome1, pop1, area1, pib1, pontos1);
+    printf("\n%s\nPopulação: %ld milhões\nÁrea: %.2f milhões de km²\nPIB: %.2f trilhões\nPontos turísticos: %d\n",
+           nome2, pop2, area2, pib2, pontos2);
 
     return 0;
 }
